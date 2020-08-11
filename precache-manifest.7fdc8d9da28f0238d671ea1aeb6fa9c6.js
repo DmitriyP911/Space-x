99cf5c452a7b06dac441002d2a2aa6af461052ea
Dmitriy Pismenniy <dima30655@gmail.com>
2020-08-11 22:50:15 +0300

    Updates

---
+++ precache-manifest.7fdc8d9da28f0238d671ea1aeb6fa9c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe9b766596f2ff4a84672bdb8eebd5e",
+    "url": "/Space-x/index.html"
+  },
+  {
+    "revision": "39166aea8d1d4f97f17c",
+    "url": "/Space-x/static/css/main.d4a68bed.chunk.css"
+  },
+  {
+    "revision": "2f438f1df31ed7007c83",
+    "url": "/Space-x/static/js/2.91002671.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Space-x/static/js/2.91002671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39166aea8d1d4f97f17c",
+    "url": "/Space-x/static/js/main.a0c58612.chunk.js"
+  },
+  {
+    "revision": "708c5718ac916cda9d5d",
+    "url": "/Space-x/static/js/runtime-main.774862cf.js"
+  },
+  {
+    "revision": "18fe86f4316b058544dddc948ca90888",
+    "url": "/Space-x/static/media/background.18fe86f4.jpg"
+  }
+]);
